d7f8c11f8c9a1f5c98b67e6d392a149125027f3a
zhumagazyyassin <yassinzhumagazy13@gmail.com>
2026-02-28 02:39:06 +0500

    Updated settings.py for production

--- config/settings.py
+++ config/settings.py
@@ -2,12 +2,57 @@ import os
 from pathlib import Path
 import dj_database_url  #requirements.txt-те болуы керек
 
+# Жобаның негізгі папкасы
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Қауіпсіздік баптаулары (Render environment variables-тен оқылады)
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-key')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = ['*']
+
+# Қолданбалар тізімі (Өз қолданбаларыңызды қосыңыз)
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    # Қолданбаларыңыз:
+    # 'myapp', 
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', # WhiteNoise қосу
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# МАҢЫЗДЫ: Мұны өз жобаңыздың атымен ауыстырыңыз
+ROOT_URLCONF = 'instagram_clone.urls' 
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'config.wsgi.application'
 
 # Дерекқор баптаулары (Render POSTGRESQL үшін)
 DATABASES = {
@@ -21,3 +66,7 @@ DATABASES = {
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Медиа файлдар (суреттер)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
