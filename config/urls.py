00a6d9fe060a486ee483db4945bf817820152d95
zhumagazyyassin <yassinzhumagazy13@gmail.com>
2026-02-28 10:01:58 +0500

    Add homepage response to fix 404

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+# Бас бетке арналған функция
+def home(request):
+    return HttpResponse("Instagram Clone API жұмыс істеп тұр! Деректерді алу үшін /api/ мекенжайын қолданыңыз.")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  # ОСЫ ЖЕР: 'admin.site.urls' болуы керек, 'admin.site.register' емес!
+    path('admin/', admin.site.urls),
+    path('', home), # Бас бетке функцияны қостық
     path('', include('pages.urls')),
 ]
