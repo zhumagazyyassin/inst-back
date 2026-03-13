ee957c22db7b95bc5ee8722095b0ffc48d38ef6b
zhumagazyyassin <yassinzhumagazy13@gmail.com>
2026-03-14 02:19:46 +0500

    Fix serializers and urls

--- pages/serializers.py
+++ pages/serializers.py
@@ -6,14 +6,23 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'username', 'email', 'bio', 'avatar_url']
 
+class LikeSerializer(serializers.ModelSerializer):
+    user_name = serializers.ReadOnlyField(source='user.username')
+    class Meta:
+        model = Like
+        fields = ['id', 'user', 'user_name', 'post']
+
 class PostSerializer(serializers.ModelSerializer):
     author_name = serializers.ReadOnlyField(source='author.username')
     likes_count = serializers.IntegerField(source='likes.count', read_only=True)
+    is_liked = serializers.SerializerMethodField()
+
     class Meta:
         model = Post
-        fields = ['id', 'author', 'author_name', 'caption', 'likes_count', 'created_at']
+        fields = ['id', 'author', 'author_name', 'caption', 'likes_count', 'is_liked', 'created_at']
 
-class LikeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Like
-        fields = '__all__'
+    def get_is_liked(self, obj):
+        user = self.context.get('request').user
+        if user and user.is_authenticated:
+            return obj.likes.filter(user=user).exists()
+        return False
