e963b85029609403a9b7d870b813c31a2a385d60
zhumagazyyassin <yassinzhumagazy13@gmail.com>
2026-03-14 02:19:18 +0500

    Fix serializers and urls

--- pages/admin.py
+++ pages/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
-from .models import User, Post, Media, Follow, Comment, Like
+from .models import User, Post, Like
 
 admin.site.register(User)
 admin.site.register(Post)
-admin.site.register(Media)
-admin.site.register(Follow)
-admin.site.register(Comment)
 admin.site.register(Like)
